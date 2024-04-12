aded085e996369ab27b2d4cf4c63a9cda26f1eed
Emery Chikezie Ogechukwu <emerychikezie2@gmail.com>
2024-04-12 15:02:12 +0100

    configured the site for prod using IIFE

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 import "./style.css";
 import { getVideo, drawVideo } from "./src/camera";
 
-const video = await getVideo();
-const canvas = document.querySelector("canvas");
-const button = document.getElementById("camera");
 
-button.addEventListener("click", () => {
-  drawVideo(video, canvas);
-});
+
+
+
+(async function run() {
+  const video = await getVideo();
+  const canvas = document.querySelector("canvas");
+  const button = document.getElementById("camera");
+
+  button.addEventListener("click", () => {
+    drawVideo(video, canvas);
+  });
+})();
+
+
 
 // await video.play();
